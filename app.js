f85abb419af9bc2cc5baf48467dcca5cfe1246f7
ahmet-parlak <ahmet--parlak@outlook.com>
2023-09-20 22:35:04 +0300

    Add Mongoose, Create Post Model
    - Integrated Mongoose into the project for database operations.
    - Designed the Post model to store post-related data.
    - Implemented post creation functionality.
    - Enabled the display of posts on the home page.

--- app.js
+++ app.js
@@ -1,22 +1,39 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
+const Post = require('./models/Post');
+
+//App
 const app = express();
+
+//DB CONNECTION
+mongoose
+  .connect('mongodb://localhost:27017/cleanblog-test-db')
+  .then(() => console.log('MongoDB connection successful!'));
 
 //TEMPLATE ENGINE
 app.set('view engine', 'ejs');
 
 //MIDDLEWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //ROUTES
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const posts = await Post.find({});
+  console.log(posts);
+  res.render('index', { posts });
 });
 app.get('/post', (req, res) => {
   res.render('post');
 });
 app.get('/add-post', (req, res) => {
   res.render('add_post');
+});
+app.post('/add-post', async (req, res) => {
+  await Post.create(req.body);
+  res.redirect('/');
 });
 app.get('/about', (req, res) => {
   res.render('about');
